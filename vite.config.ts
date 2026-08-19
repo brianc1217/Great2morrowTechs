2410b92d997a201ad0d6cd0103e0daef32cdab79
Brian Carson <great2morrowtravel@gmail.com>
2026-08-19 10:50:33 -0400

    Deploy Great 2morrow Techs with GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Great2morrowTechs/",
   plugins: [
     react(),
     tailwindcss(),
